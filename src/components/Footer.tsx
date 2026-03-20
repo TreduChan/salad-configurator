e34ee582a7fc7a0d7fef84f9808fe9086312c5fd
e2402703 <e2402703@vamk.fi>
2026-03-20 14:45:21 +0200

    Task 1.6: Footer #6

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function footer() {
+    return (
+        <header className="bg-[#A2D135] text-black p-8 mt-12 w-full flex flex-wrap justify-around items-start">
+            <div className="Contact">
+                <p>+123 1323213123</p>
+            </div>
+            <div className="Social Media">
+                <p>PokeMedia</p>
+            </div>
+            <div className="Branding">
+                <p>Pokemon</p>
+            </div>
+        </header>
+    );
+}
