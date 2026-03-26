0db22ba9d053a377711d9b534adedd371ee1b83d
TreduChan <P201322.Jeesus@gmail.com>
2026-03-26 20:22:34 +0200

    Assemble parts in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import BowlSelection from './components/BowlSelection'
+import CenterBowl from './components/CenterBowl'
+import BaseSelection from './components/BaseSelection'
+import IngredientSection from './components/IngredientSection'
+import SummaryBar from './components/SummaryBar'
 import './App.css'
 
 function App() {
-
-
   return (
-    <>
-      <div className='min-h-screen flex flex-col bg-white font-sans'>
-        <Header />
-        <main className='flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4'>
-        </main>
-        <Footer />
-      </div>
+    <div className='min-h-screen flex flex-col bg-white font-sans'>
+      <Header />
 
-    </>
+      <main className='flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4'>
+        
+        <div className="flex flex-col lg:flex-row gap-6 justify-between items-stretch">
+          <BowlSelection />
+          <CenterBowl />
+          <BaseSelection />
+        </div>
+
+        <IngredientSection />
+
+        <SummaryBar />
+
+      </main>
+
+      <Footer />
+    </div>
   )
 }
 
