90219fce1ea31294a175426252bc307db2d5bfb2
e2403290 <e2403290@edu.vamk.fi>
2026-03-27 09:50:36 +0200

    Task 2.11: Mapping Bases

--- src/components/BaseSelection.tsx
+++ src/components/BaseSelection.tsx
@@ -1,17 +1,24 @@
-const BaseSelection = () => {
+type Ingredients = {
+  id: number;
+  name: string;
+}
+
+type Props = {
+  base: Ingredients[];
+}
+
+const BaseSelection = ({ base } : Props) => {
     return (
     <div className="bg-zinc-800 rounded-[3rem] p-6 text-white w-full lg:w-1/4 flex flex-col items-center shadow-lg">
 
     <div className="bg-white text-black font-bold rounded-full w-8 h-8 flex items-center justify-center mb-4 shrink-0">2</div>
     
     <div className="w-full space-y-3">
-        <div className="border-b border-gray-600 pb-2 flex justify-end gap-4 items-center">
-          {/* empty placeholder */}
-        </div>
-
-        <div className="border-b border-gray-600 pb-2 flex justify-end gap-4 items-center">
-          {/* empty placeholder */}
-        </div>
+      {base.map((base) => (
+        <button key={base.id} className="border-b border-gray-600 pb-2 flex justify-end gap-4 items-center">
+          {base.name}
+        </button>
+      ))}
         </div>
     </div>
     );
