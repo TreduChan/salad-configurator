23f20f3ece9c18d8d105817354cae65960072f6d
e2402703 <e2402703@vamk.fi>
2026-03-27 09:24:25 +0200

    Refactor Configurator component to include state management for bowls, categories, and ingredients

--- src/pages/Configurator.tsx
+++ src/pages/Configurator.tsx
@@ -1,8 +1,19 @@
-import BowlSelection from '../components/BowlSelection'
-import CenterBowl from '../components/CenterBowl'
-import BaseSelection from '../components/BaseSelection'
+import { useState } from "react";
+import BowlSelection from '../components/BowlSelection';
+import CenterBowl from '../components/CenterBowl';
+import BaseSelection from '../components/BaseSelection';
 
 export default function Configurator() {
+    type Bowl = any;
+    type Category = any;
+    type Ingredient = any;
+    const [bowls, setBowls] = useState<Bowl[]>([]);
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+    //For peace and mind delete later
+    void bowls, setBowls;
+    void categories, setCategories;
+    void ingredients, setIngredients;
     return (
         <div className="flex flex-col lg:flex-row gap-6 justify-between items-stretch">
             <BowlSelection />
