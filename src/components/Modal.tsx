ed40f5d4dd7e909f1a1a1a0ea18521ce2b689088
e2403290 <e2403290@edu.vamk.fi>
2026-04-10 10:11:37 +0300

    Task 4.2: Modal component's basic structure #72

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,34 +4,16 @@ type ModalProps = {
     onClose: () => void;
     children: ReactNode;
 }
-
-
 export default function Modal({isOpen, onClose, children}) {
     if (!isOpen) return null;
      return (
-    <div style={overlayStyle}>
-      <div style={modalStyle}>
-        <button onClick={onClose}>Close</button>
+    <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50" onClick={onClose}>
+      <div className="bg-white p-6 rounded-lg shadow-lg relative" onClick={(e) => e.stopPropagation()}>
+        <button onClick={onClose} className="absolute top-2 right-2 text-gray-600 hover:text-black">X</button>
         {children}
       </div>
     </div>
   );
 }
 
-const overlayStyle: React.CSSProperties = {
-  position: "fixed",
-  top: 0,
-  left: 0,
-  width: "100%",
-  height: "100%",
-  backgroundColor: "rgba(0, 0, 0, 0.5)",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-};
 
-const modalStyle: React.CSSProperties = {
-  background: "white",
-  padding: "20px",
-  borderRadius: "8px",
-};
