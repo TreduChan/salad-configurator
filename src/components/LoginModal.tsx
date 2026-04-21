fce4c127a3e2492c9b4c767a1c66f91b371d01f8
Joni <e2403290@edu.vamk.fi>
2026-04-21 16:39:57 +0300

    Task 4.4: Building the LoginModal component #74

---
+++ src/components/LoginModal.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Modal from "./Modal";
+
+export default function LoginModal() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <Modal>
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </Modal>
+  );
+}
